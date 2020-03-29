187ab890bd97484f88173cfafe8dbea89703101d
noza8810 <byby8810@gmail.com>
2020-03-29 06:14:08 +0000

    ユーザーの削除機能を実装

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -41,8 +41,24 @@ module SessionsHelper
     end
   end
   
+  # 渡されたユーザーがログイン済みのユーザーであればtrueを返す
+  def current_user?(user)
+    user == current_user
+  end
+  
   #現在ログイン中のユーザーがいればtrue、そうでなければfalseを返す。
   def logged_in?
     !current_user.nil?
+  end
+  
+  #記憶しているURLにリダイレクトする
+  def redirect_back_or(default_url)
+    redirect_to(session[:forwarding_url] || default_url)
+    session.delete(:forwarding_url)
+  end
+  
+  # アクセスしようとしたURLを記憶する
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
