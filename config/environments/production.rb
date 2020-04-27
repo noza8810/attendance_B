4127c76976ef890b7bddc5735557772e7b496d01
noza8810 <byby8810@gmail.com>
2020-04-27 09:40:48 +0000

    Update Gemfile for Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -87,5 +87,5 @@ Rails.application.configure do
   end
 
   # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = true
+  config.active_record.dump_schema_after_migration = false
 end
